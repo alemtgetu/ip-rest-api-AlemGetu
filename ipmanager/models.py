a7ff74522aa33d91606ea31bfd6cc13d3c6c828e
Alem Getu <alem.t.getu@gmail.com>
2020-09-16 13:52:43 -0400

    add CidrBlock and IpAddress models

--- ipmanager/models.py
+++ ipmanager/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class CidrBlock(models.Model):
+
+    cidr_block = models.CharField(max_length=20)
+
+
+class IpAddress(models.Model):
+
+    cidr_block = models.ForeignKey(
+        CidrBlock, related_name='ips', on_delete=models.CASCADE)
+    ip_address = models.GenericIPAddressField(protocol='IPv4')
+    status = models.CharField(max_length=20, default='Available')
