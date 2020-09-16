7b66c3b9db383a19e2755b7d606a031eba89ee3b
Alem Getu <alem.t.getu@gmail.com>
2020-09-16 15:27:11 -0400

    add status choice for IpAddress model satus field

--- ipmanager/models.py
+++ ipmanager/models.py
@@ -12,4 +12,9 @@ class IpAddress(models.Model):
     cidr_block = models.ForeignKey(
         CidrBlock, related_name='ips', on_delete=models.CASCADE)
     ip_address = models.GenericIPAddressField(protocol='IPv4')
-    status = models.CharField(max_length=20, default='Available')
+    STATUS_CHOICES = (
+        ('AC', 'Acquired'),
+        ('AV', 'Available'),
+    )
+    status = models.CharField(
+        max_length=2, choices=STATUS_CHOICES, default='AV')
