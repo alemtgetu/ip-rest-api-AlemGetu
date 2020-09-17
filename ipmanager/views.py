63e04e554859b2606fcd2a3396fd9c67b10c566a
Alem Getu <alem.t.getu@gmail.com>
2020-09-17 11:29:39 -0400

    update cidr viewset not to duplicate cidrBlock

--- ipmanager/views.py
+++ ipmanager/views.py
@@ -26,9 +26,32 @@ class CidrBlockViewSet(viewsets.ModelViewSet):
 
             # create CIDR model object with the cidr input from request and
             # with the netmask from CIDR IPv4Network object
-            cidr_model_obj = CidrBlock.objects.create(
-                cidr_block=cidr, netmask=str(cidr_net_obj.netmask))
+            # cidr_model_obj = CidrBlock.objects.create(cidr_block=cidr, netmask=str(cidr_net_obj.netmask))'''
 
+            ''' using get object by CIDR gives 
+                sqlite InterfaceError: Error binding parameter 0 - probably unsupported type
+            
+            try:
+                cidr_model_obj = CidrBlock.objects.get(cidr_block=cidr)
+                #cidr_model_obj.netmask = str(cidr_net_obj.netmask)
+                return Response('CIDR already EXIST', status=status.HTTP_201_CREATED)
+            except CidrBlock.DoesNotExist:
+                cidr_model_obj = CidrBlock(
+                    cidr_block=cidr, netmask=str(cidr_net_obj.netmask))
+                cidr_model_obj.save()'''
+            # work around
+            cidr_model_obj = CidrBlock.objects.raw(
+                'SELECT * FROM ipmanager_cidrblock where cidr_block='+'"'+cidr+'"')
+            if (cidr_model_obj.__len__() > 0):
+                return Response('CIDR already EXIST', status=status.HTTP_208_ALREADY_REPORTED)
+            else:
+                cidr_model_obj = CidrBlock(
+                    cidr_block=cidr, netmask=str(cidr_net_obj.netmask))
+                cidr_model_obj.save()
+            ##################
+
+            # create all IPs in the CIDR block
+            # the IPs will be stored in IPAddress Table
             for ip in list(cidr_net_obj.hosts()):
                 '''try:
                     ip_add = IpAddress.objects.get(ip_address=str(ip))
@@ -38,7 +61,7 @@ class CidrBlockViewSet(viewsets.ModelViewSet):
                         ip), cidr_block=cidr_model_obj)
                 ip_add.save()'''
                 ip_add = IpAddress.objects.get_or_create(ip_address=str(
-                    ip), cidr_block=cidr_model_obj)
+                    ip), cidr_block_id=cidr_model_obj)
             return Response('Succesfully created CIDR and all IPs in the block', status=status.HTTP_201_CREATED)
         return Response('Invalid CIDR input', status=status.HTTP_400_BAD_REQUEST)
 
